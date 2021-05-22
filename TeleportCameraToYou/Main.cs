37ee2f18c5b07f16bf4dd1d875884d13488810aa
Nirv-git <81605232+Nirv-git@users.noreply.github.com>
2021-05-21 19:36:23 -0500

    TeleportCamToU

---
+++ TeleportCameraToYou/Main.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using UIExpansionKit.API;
+using UnityEngine;
+
+
+
+[assembly: MelonInfo(typeof(LocalCamera.Main), "TeleportCameraToYou", "1.0.1", "Nirvash")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace LocalCamera
+{
+    public class Main : MelonMod
+    {
+
+        public override void OnApplicationStart()
+        {
+            MelonPreferences.CreateCategory("TeleportCamToYou", "Teleport Camera To You");
+            MelonPreferences.CreateEntry("TeleportCamToYou", "Distance", 0.1f, "Distance from you");
+
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.CameraQuickMenu).AddSimpleButton("Teleport Camera To You", () =>
+            {
+                GameObject vrcam = GameObject.Find("TrackingVolume/PlayerObjects/UserCamera/ViewFinder");
+                if (vrcam is null) return;
+                VRCPlayer player = VRCPlayer.field_Internal_Static_VRCPlayer_0;
+                Vector3 pos = GameObject.Find(player.gameObject.name + "/AnimationController/HeadAndHandIK/HeadEffector").transform.position + (player.transform.forward * MelonPreferences.GetEntryValue<float>("TeleportCamToYou", "Distance"));
+                vrcam.transform.position = pos;
+                
+            });
+        }
+
+    }
+}
+
---
+++ TeleportCameraToYou/README.md
@@ -0,0 +1,9 @@
+**USE IT AT YOUR OWN RISK. Modding the client is against VRChat's ToS. I am not responsible for any bans or any punishments you may get by using these mods**
+
+**This was made in my free time, and is provided AS IS without warranty of any kind, express or implied. Any use is at your own risk and you accept all responsibility**
+
+
+# VRC-TeleportCameraToYou
+Another ultra simple mod, this puts a button on the UIX _Camera_ QuickMenu that teleports the Viewfinder of your camera right in front of your face. This is useful if you left it somewhere far away, or are using a small avatar. 
+
+In mod settings there is one preference, the distance from your head the camera gets teleported to. 
