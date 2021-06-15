af068d5675682441810778cfac396172dcd2835a
Nirv-git <81605232+Nirv-git@users.noreply.github.com>
2021-06-14 20:39:34 -0500

    Update Main.cs
    Cleaned up code,
    Made Toggle button state set from the start instead of waiting for the button to exist
    
    Removed the delay button feature, as it doesnt work with ML0.4.0

--- ImmobilizePlayerMod/Main.cs
+++ ImmobilizePlayerMod/Main.cs
@@ -4,7 +4,7 @@ using UIExpansionKit.API;
 using UnityEngine;
 
 
-[assembly: MelonInfo(typeof(ImmobilizePlayer.Main), "ImmobilizePlayerMod", "0.3.6", "Nirvash")]
+[assembly: MelonInfo(typeof(ImmobilizePlayer.Main), "ImmobilizePlayerMod", "0.3.7", "Nirvash")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace ImmobilizePlayer
@@ -12,60 +12,47 @@ namespace ImmobilizePlayer
     public class Main : MelonMod
     {
         public bool buttonEnabled;
-        public bool delayButton;
-        public bool firstRun = true;
+        //public bool delayButton;
         public bool imState = false;
         public GameObject buttonQM;
 
         public override void OnApplicationStart()
         {
             RegisterModPrefs();
-
-            if (delayButton) MelonCoroutines.Start(SetupUI());//MelonCoroutines.Start(SetupUI());
-            else AddToQM();
-
-            //ExpansionKitApi.RegisterWaitConditionBeforeDecorating(SetupUI());
-            ExpansionKitApi.OnUiManagerInit += UiManagerInit;
+            //if (delayButton) //Dirty hack broke with ML 0.4.0
+            //{
+            //    //MelonCoroutines.Start(SetupUI());
+            //    ExpansionKitApi.RegisterWaitConditionBeforeDecorating(SetupUI()); //This doesnt work for positioning the order of the buttons past a point
+            //}
+            //else AddToQM();
+            AddToQM();
         }
 
         private void AddToQM()
         {
-            //MelonLogger.Msg($"Adding QM button");
+            MelonLogger.Msg($"Adding QM button");
             ExpansionKitApi.GetExpandedMenu(ExpandedMenu.QuickMenu).AddToggleButton("Immobilize", (action) =>
             {
                 imState = !imState;
                 VRCPlayer.field_Internal_Static_VRCPlayer_0.prop_VRCPlayerApi_0.Immobilize(imState);
             }
-            , () => imState, (button) => buttonQM = button);
-
-            firstRun = false;
+            , () => imState, (button) => {buttonQM = button; button.gameObject.SetActive(buttonEnabled); });
         }
 
-        private IEnumerator SetupUI()//I want the button to be last on the list
-        {
-            while (QuickMenu.prop_QuickMenu_0 == null) yield return null;
-            yield return new WaitForSeconds(13f);
-            MelonLogger.Msg($"Adding QM button late and reloading the menu");
-            AddToQM();
-            MelonPreferences.Save();
-        }
-
-        public void UiManagerInit()
-        {
-            MelonCoroutines.Start(ButtonState());
-        }
-
-        private IEnumerator ButtonState()
-        {//Give UIX a few seconds to make buttons, then set the state of the Toggle 
-            yield return new WaitForSeconds(5f);
-            OnPreferencesSaved();
-        }
+        //private IEnumerator SetupUI()//I want the button to be last on the list
+        //{
+        //    while (QuickMenu.prop_QuickMenu_0 == null) yield return null;
+        //    yield return new WaitForSeconds(15f);
+        //    MelonLogger.Msg($"Adding QM button late");
+        //    AddToQM();
+        //    //MelonPreferences.Save();
+        //}
 
         private void RegisterModPrefs()
         {
             MelonPreferences.CreateCategory("ImPlaMod", "Immobilize Player Mod");
             MelonPreferences.CreateEntry<bool>("ImPlaMod", "QuickMenuButton", true, "Put Button on Quick Menu");
-            MelonPreferences.CreateEntry<bool>("ImPlaMod", "DelayButton", true, "Delay button position on Menu");
+            //MelonPreferences.CreateEntry<bool>("ImPlaMod", "DelayButton", true, "Delay button position on Menu");
 
             OnPreferencesSaved();
         }
@@ -73,21 +60,13 @@ namespace ImmobilizePlayer
         public override void OnPreferencesSaved()
         {
             buttonEnabled = MelonPreferences.GetEntryValue<bool>("ImPlaMod", "QuickMenuButton");
-            delayButton = MelonPreferences.GetEntryValue<bool>("ImPlaMod", "DelayButton");
+            //delayButton = MelonPreferences.GetEntryValue<bool>("ImPlaMod", "DelayButton");
 
-            if (!firstRun && buttonEnabled && buttonQM != null)
+            if (buttonQM != null)
             {
-                buttonQM.active = true;
+                buttonQM.SetActive(buttonEnabled);
                 //MelonLogger.Msg($"QM Button to active");
-            }
-            if (!firstRun && !buttonEnabled && buttonQM != null)
-            {
-                buttonQM.active = false;
-                //MelonLogger.Msg($"QM Button to disabled");
-            }
-            
+            }  
         }
-
-
     }
 }
