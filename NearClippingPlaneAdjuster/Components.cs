7e17427d850ee7db41f4d9cf8e0cf9eac45f53de
Nirv-git <81605232+Nirv-git@users.noreply.github.com>
2021-06-16 18:15:20 -0500

    NearClip 1.38
    Fixes bug where the Nearclip value would be changed twice on world load
    
    Added an option where if you are using the smaller default nearclip (0.001) the mod will raise it to 0.01 when the Quickmenu is open. This can be useful as some worlds case buttons to be hard to press at that value.

---
+++ NearClippingPlaneAdjuster/Components.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnhollowerBaseLib.Attributes;
+using UnityEngine;
+
+namespace NearClipPlaneAdj.Components
+{
+    // Stolen from knah
+    // https://github.com/knah/VRCMods/blob/326b5f6d3d1c4bc3474b3518938d49efb918c1d8/UIExpansionKit/Components/EnableDisableListener.cs
+    //Then I  stole this from https://github.com/tetra-fox/QMFreeze/blob/c3088fcda4f9d29f797d3613d3ea74b48e6f268c/QMFreeze/Components.cs#L9
+    public class EnableDisableListener : MonoBehaviour
+    {
+#nullable enable
+
+        [method: HideFromIl2Cpp]
+        public event Action? OnEnabled;
+
+        [method: HideFromIl2Cpp]
+        public event Action? OnDisabled;
+
+#nullable disable
+
+        public EnableDisableListener(IntPtr obj0) : base(obj0)
+        {
+        }
+
+        private void OnEnable()
+        {
+            OnEnabled?.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            OnDisabled?.Invoke();
+        }
+    }
+}
--- NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
+++ NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
@@ -3,8 +3,10 @@ using MelonLoader;
 using UnityEngine;
 using VRC.UserCamera;
 using ConsoleColor = System.ConsoleColor;
+using UnhollowerRuntimeLib;
+using NearClipPlaneAdj.Components;
 
-[assembly: MelonInfo(typeof(NearClipPlaneAdj.NearClipPlaneAdjMod), "NearClipPlaneAdj", "1.35", "Nirvash")]
+[assembly: MelonInfo(typeof(NearClipPlaneAdj.NearClipPlaneAdjMod), "NearClipPlaneAdj", "1.38", "Nirvash")]
 [assembly: MelonGame("VRChat", "VRChat")]
 
 namespace NearClipPlaneAdj
@@ -14,39 +16,81 @@ namespace NearClipPlaneAdj
     {
         public bool keybindsEnabled;
         public bool smallerDefault;
+        public float oldNearClip;
+        public bool raiseNearClip;
         public override void OnApplicationStart()
         {
             MelonPreferences.CreateCategory("NearClipAdj", "NearClipPlane Adjuster");
             MelonPreferences.CreateEntry<bool>("NearClipAdj", "Keyboard", true, "Keyboard Shortcuts: '[' - 0.0001, ']' - 0.05");
             MelonPreferences.CreateEntry<bool>("NearClipAdj", "SmallerDefault", false, "Smaller Default Nearclip on World Change - 0.001 vs 0.01");
+            MelonPreferences.CreateEntry<bool>("NearClipAdj", "RaiseOnQuickMenu", false, "If using smaller Default Nearclip (0.001) raise to 0.01 when Quick Menu opens.");
             OnPreferencesSaved();
 
-            ExpansionKitApi.RegisterSimpleMenuButton(ExpandedMenu.SettingsMenu, "Nearplane-0.05", (() => ChangeNearClipPlane(.05f)));
-            ExpansionKitApi.RegisterSimpleMenuButton(ExpandedMenu.SettingsMenu, "Nearplane-0.01", (() => ChangeNearClipPlane(.01f)));
-            ExpansionKitApi.RegisterSimpleMenuButton(ExpandedMenu.SettingsMenu, "Nearplane-0.001", (() => ChangeNearClipPlane(.001f)));
-            ExpansionKitApi.RegisterSimpleMenuButton(ExpandedMenu.SettingsMenu, "Nearplane-0.0001", (() => ChangeNearClipPlane(.0001f)));
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.05", (() => ChangeNearClipPlane(.05f, true)));
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.01", (() => ChangeNearClipPlane(.01f, true)));
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.001", (() => ChangeNearClipPlane(.001f, true)));
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.0001", (() => ChangeNearClipPlane(.0001f, true)));
             MelonLogger.Msg("Near Plane Adjust Init");
+
+            MelonLogger.Msg("Registering components...");
+            ClassInjector.RegisterTypeInIl2Cpp<EnableDisableListener>();
+            ExpansionKitApi.OnUiManagerInit += UiManagerInit;
         }
 
         public override void OnPreferencesSaved()
         {
             keybindsEnabled = MelonPreferences.GetEntryValue<bool>("NearClipAdj", "Keyboard");
             smallerDefault = MelonPreferences.GetEntryValue<bool>("NearClipAdj", "SmallerDefault");
-
+            raiseNearClip = MelonPreferences.GetEntryValue<bool>("NearClipAdj", "SmallerDefault");
         }
 
-        private void ChangeNearClipPlane(float value)
+        public void UiManagerInit()
+        {
+            MelonLogger.Msg("Adding QM listener..."); //From https://github.com/tetra-fox/QMFreeze/blob/master/QMFreeze/Mod.cs
+            // MicControls is enabled no matter the QM page that's open, so let's use that to determine whether or not the QM is open
+            // Unless you have some other mod that removes this button then idk lol
+            EnableDisableListener listener = GameObject.Find("/UserInterface/QuickMenu/MicControls").AddComponent<EnableDisableListener>();
+            listener.OnEnabled += delegate { QMopen(); };
+            listener.OnDisabled += delegate { QMclosed(); };
+
+            MelonLogger.Msg("Initialized!");
+        }
+
+        public void QMopen()
+        { if (!raiseNearClip) return;
+            var screenCamera = GetScreenCam();
+            if (screenCamera is null) return;
+            float clipValue = screenCamera.nearClipPlane;
+            if (clipValue < .0011f && clipValue > .0009f)
+                ChangeNearClipPlane(.01f, false);
+            oldNearClip = clipValue;
+        }
+
+        public void QMclosed()
+        { if (!raiseNearClip) return;
+            var screenCamera = GetScreenCam();
+            if (screenCamera is null) return;
+            float clipValue = screenCamera.nearClipPlane;
+            if (clipValue != oldNearClip)
+                ChangeNearClipPlane(oldNearClip, false);
+        }
+
+        private Camera GetScreenCam() 
         {
             VRCVrCamera vrCamera = VRCVrCamera.field_Private_Static_VRCVrCamera_0;
-            if (!vrCamera)
-                return;
-            Camera screenCamera = vrCamera.field_Public_Camera_0;
-            //Camera screenCamera = vrCamera.screenCamera;
-            if (!screenCamera)
-                return;
+            if (!vrCamera) return null;
+            Camera screenCamera = vrCamera.field_Public_Camera_0; //Camera screenCamera = vrCamera.screenCamera;
+            if (!screenCamera) return null;
+            return screenCamera;
+        }
+
+        private void ChangeNearClipPlane(float value, bool printMsg)
+        {
+            var screenCamera = GetScreenCam();
+            if (screenCamera is null) return;
             float oldvalue = screenCamera.nearClipPlane;
             screenCamera.nearClipPlane = value;
-            MelonLogger.Msg($"Near plane previous: {oldvalue}, Now: {value} {(keybindsEnabled ? "- Keyboard Hotkeys: '[' - 0.0001, ']' - 0.05" : "" )}");
+            if(printMsg) MelonLogger.Msg($"Near plane previous: {oldvalue}, Now: {value} {(keybindsEnabled ? "- Keyboard Hotkeys: '[' - 0.0001, ']' - 0.05" : "" )}");
             ChangePhotoCameraNearField(value);
         }
 
@@ -62,25 +106,31 @@ namespace NearClipPlaneAdj
 
         }
 
-        public override void OnLevelWasLoaded(int level)
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
+            //MelonLogger.Msg($"index: {buildIndex} sceneName: {sceneName}");
             //Return the clipping distance to a safe, close value on load
-            switch (level)//Without switch this would run 3 times at world load
+            switch (buildIndex)//Without switch this would run 3 times at world load
             {
                 case 0: //App
+                    break;
                 case 1: //ui
+                    break;
+                case 2: //empty
                     break;
                 default:
                     MelonCoroutines.Start(SetNearClipPlane(0.01f));
                     break;
             }
+
         }
 
         System.Collections.IEnumerator SetNearClipPlane(float znear)
         {
             yield return new WaitForSecondsRealtime(15); //Wait 15 seconds after world load before setting the clipping value. Waiting for the next/first frame does not work
             if (smallerDefault) znear = 0.001f;
-            ChangeNearClipPlane(znear);
+            ChangeNearClipPlane(znear, true);
+            oldNearClip = znear;
             MelonLogger.Msg("Near plane adjusted after world load");
         }
 
@@ -92,12 +142,12 @@ namespace NearClipPlaneAdj
             if (Input.GetKeyDown(KeyCode.LeftBracket))
             {
                 MelonLogger.Msg(ConsoleColor.DarkCyan, "NearClip set to smallest value on keypress: 0.0001");
-                ChangeNearClipPlane(.0001f);
+                ChangeNearClipPlane(.0001f, true);
             }
             if (Input.GetKeyDown(KeyCode.RightBracket))
             {
                 MelonLogger.Msg(ConsoleColor.DarkCyan, "NearClip set to largest value on keypress: 0.05");
-                ChangeNearClipPlane(.05f);
+                ChangeNearClipPlane(.05f, true);
             }
         }
 
