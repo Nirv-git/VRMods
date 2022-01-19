450180d19f681afda04b24a368f6a06abde7490a
Nirv-git <81605232+Nirv-git@users.noreply.github.com>
2022-01-18 21:26:23 -0600

    NearClip Updates
    Added options for AMAPI
    Added option to move UIX buttons to Settings QM
    Added option to adjust the UI Camera

---
+++ NearClippingPlaneAdjuster/ActionMenu.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.IO;
+using ActionMenuApi.Api;
+using System.Collections.Generic;
+
+namespace NearClipPlaneAdj
+{
+    public class CustomActionMenu
+    {
+        public static Texture2D n05, n01, n001, n0001, plane;
+
+        public static void loadAssets()
+        {
+            n05 = LoadEmbeddedImages("n05.png");
+            n01 = LoadEmbeddedImages("n01.png");
+            n001 = LoadEmbeddedImages("n001.png");
+            n0001 = LoadEmbeddedImages("n0001.png");
+            plane = LoadEmbeddedImages("plane.png");
+        }
+
+        private static Texture2D LoadEmbeddedImages(string imageName)
+        {
+            try
+            {
+                //Load image into Texture
+                using var assetStream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("NearClippingPlaneAdjuster.Icons." + imageName);
+                using var tempStream = new MemoryStream((int)assetStream.Length);
+                assetStream.CopyTo(tempStream);
+                var Texture2 = new Texture2D(2, 2);
+                ImageConversion.LoadImage(Texture2, tempStream.ToArray());
+                Texture2.wrapMode = TextureWrapMode.Clamp;
+                Texture2.hideFlags |= HideFlags.DontUnloadUnusedAsset;
+                return Texture2;
+            }
+            catch (System.Exception ex) { NearClipPlaneAdjMod.Logger.Error("Failed to load image from asset bundle: " + imageName + "\n" + ex.ToString()); return null; }
+        }
+
+
+        public static void InitUi()
+        {
+            loadAssets();
+
+            if (NearClipPlaneAdjMod.amapi_ModsFolder.Value)
+                AMUtils.AddToModsFolder("<color=#ff00ff>NearClip Plane Adj</color>", () => AMsubMenu(), plane);
+            else
+                VRCActionMenuPage.AddSubMenu(ActionMenuPage.Options, "<color=#ff00ff>NearClip Plane Adj</color>", () => AMsubMenu(), plane);
+        }
+
+        private static void AMsubMenu()
+        {
+            foreach (var clip in clipList)
+            { 
+                CustomSubMenu.AddButton(clip.Key.ToString(), () =>
+                {
+                    NearClipPlaneAdjMod main = new NearClipPlaneAdjMod(); main.ChangeNearClipPlane(clip.Key, true);
+                }, clip.Value);
+            }
+        }
+
+        public static Dictionary<float, Texture2D> clipList = new Dictionary<float, Texture2D> {
+                { .05f, n05 },
+                { .01f, n01 },
+                { .001f, n001 },
+                { .0001f, n0001 }
+            };
+    }
+}
--- NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
+++ NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
@@ -6,48 +6,93 @@ using ConsoleColor = System.ConsoleColor;
 using UnhollowerRuntimeLib;
 using NearClipPlaneAdj.Components;
 using System.Collections;
+using System.Linq;
 
-[assembly: MelonInfo(typeof(NearClipPlaneAdj.NearClipPlaneAdjMod), "NearClipPlaneAdj", "1.43", "Nirvash")]
+[assembly: MelonInfo(typeof(NearClipPlaneAdj.NearClipPlaneAdjMod), "NearClipPlaneAdj", "1.61", "Nirvash")]
 [assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonOptionalDependencies("ActionMenuApi")]
 
 namespace NearClipPlaneAdj
 {
 
     public class NearClipPlaneAdjMod : MelonMod
     {
-        public bool keybindsEnabled;
-        public bool smallerDefault;
+        public static MelonLogger.Instance Logger;
+
+        public MelonPreferences_Entry<bool> keybindsEnabled;
+        public MelonPreferences_Entry<bool> smallerDefault;
+        public MelonPreferences_Entry<bool> raiseNearClip;
+        public MelonPreferences_Entry<bool> changeUIcam;
+        public MelonPreferences_Entry<bool> AMAPI_en;
+        public static MelonPreferences_Entry<bool> amapi_ModsFolder;
+        public MelonPreferences_Entry<bool> UIX_butts_QM;
+
         public float oldNearClip;
-        public bool raiseNearClip;
+        public float lastSetNearClip;
+        public Camera UIcamera;
+
+        private GameObject n05, n01, n001, n0001, QMbutt;
+
+
         public override void OnApplicationStart()
         {
+            Logger = new MelonLogger.Instance("NearClipPlaneAdj", ConsoleColor.DarkYellow);
+
             MelonPreferences.CreateCategory("NearClipAdj", "NearClipPlane Adjuster");
-            MelonPreferences.CreateEntry<bool>("NearClipAdj", "Keyboard", true, "Keyboard Shortcuts: '[' - 0.0001, ']' - 0.05");
-            MelonPreferences.CreateEntry<bool>("NearClipAdj", "SmallerDefault", false, "Smaller Default Nearclip on World Change - 0.001 vs 0.01");
-            MelonPreferences.CreateEntry<bool>("NearClipAdj", "RaiseOnQuickMenu", false, "If using smaller Default Nearclip (0.001) raise to 0.01 when Quick Menu opens.");
-            OnPreferencesSaved();
+            keybindsEnabled = MelonPreferences.CreateEntry<bool>("NearClipAdj", "Keyboard", true, "Keyboard Shortcuts: '[' - 0.0001, ']' - 0.05");
+            smallerDefault = MelonPreferences.CreateEntry<bool>("NearClipAdj", "SmallerDefault", false, "Smaller Default Nearclip on World Change - 0.001 vs 0.01");
+            raiseNearClip = MelonPreferences.CreateEntry<bool>("NearClipAdj", "RaiseOnQuickMenu", false, "If using smaller Default Nearclip (0.001) raise to 0.01 when Quick Menu opens.");
+            changeUIcam = MelonPreferences.CreateEntry<bool>("NearClipAdj", "changeUIcam", true, "Change UI Camera Nearclip");
+            AMAPI_en = MelonPreferences.CreateEntry<bool>("NearClipAdj", "AMAPI_en", true, "Action Menu API Support (Requires Restart)");
+            amapi_ModsFolder = MelonPreferences.CreateEntry("NearClipAdj", "amapi_ModsFolder", false, "Place Action Menu in 'Mods' Sub Menu instead of 'Config' menu (Restert Required)");
+            UIX_butts_QM = MelonPreferences.CreateEntry("NearClipAdj", "UIX_butts_QM", true, "Place buttons in Settings QM instead of Settings Big Menu");
 
-            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.05", (() => ChangeNearClipPlane(.05f, true)));
-            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.01", (() => ChangeNearClipPlane(.01f, true)));
-            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.001", (() => ChangeNearClipPlane(.001f, true)));
-            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.0001", (() => ChangeNearClipPlane(.0001f, true)));
-            MelonLogger.Msg("Near Plane Adjust Init");
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.05", (() => ChangeNearClipPlane(.05f, true)), (butt) => { n05 = butt; butt.SetActive(!UIX_butts_QM.Value); });
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.01", (() => ChangeNearClipPlane(.01f, true)), (butt) => { n01 = butt; butt.SetActive(!UIX_butts_QM.Value); });
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.001", (() => ChangeNearClipPlane(.001f, true)), (butt) => { n001 = butt; butt.SetActive(!UIX_butts_QM.Value); });
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.0001", (() => ChangeNearClipPlane(.0001f, true)), (butt) => { n0001 = butt; butt.SetActive(!UIX_butts_QM.Value); });
 
-            MelonLogger.Msg("Registering components...");
+            var clips = new float[] {
+                .05f,
+                .01f,
+                .001f,
+                .0001f, };
+
+            var Menu = ExpansionKitApi.CreateCustomQuickMenuPage(LayoutDescriptionCustom.QuickMenu1Column6Row);
+            ExpansionKitApi.GetExpandedMenu(ExpandedMenu.UiElementsQuickMenu).AddSimpleButton("Near Clipping Plane Distance", () => Menu.Show(), (butt) => { QMbutt = butt; butt.SetActive(UIX_butts_QM.Value); });
+            Menu.AddLabel("Near Clipping Plane Distance");
+            foreach (var clip in clips)
+            {
+                Menu.AddSimpleButton($"Nearplane-{clip}", (() => ChangeNearClipPlane(clip, true)));
+            }
+            Menu.AddSimpleButton("Close", () => Menu.Hide());
+
+
+            Logger.Msg("Near Plane Adjust Init");
+
+            Logger.Msg("Registering components...");
             ClassInjector.RegisterTypeInIl2Cpp<EnableDisableListener>();
             MelonCoroutines.Start(OnLoad());
+
+            if (MelonHandler.Mods.Any(m => m.Info.Name == "ActionMenuApi") && AMAPI_en.Value)
+            {
+                CustomActionMenu.InitUi();
+            }
+            else Logger.Msg("ActionMenuApi is missing, or setting is toggled off in Mod Settings - Not adding controls to ActionMenu");
         }
 
         public override void OnPreferencesSaved()
         {
-            keybindsEnabled = MelonPreferences.GetEntryValue<bool>("NearClipAdj", "Keyboard");
-            smallerDefault = MelonPreferences.GetEntryValue<bool>("NearClipAdj", "SmallerDefault");
-            raiseNearClip = MelonPreferences.GetEntryValue<bool>("NearClipAdj", "SmallerDefault");
+            if (!n05?.Equals(null) ?? false) n05.SetActive(!UIX_butts_QM.Value);
+            if (!n01?.Equals(null) ?? false) n01.SetActive(!UIX_butts_QM.Value);
+            if (!n001?.Equals(null) ?? false) n001.SetActive(!UIX_butts_QM.Value);
+            if (!n0001?.Equals(null) ?? false) n0001.SetActive(!UIX_butts_QM.Value);
+            if (!QMbutt?.Equals(null) ?? false) QMbutt.SetActive(UIX_butts_QM.Value);
         }
 
         public IEnumerator OnLoad()
         {
-            MelonLogger.Msg("Adding QM listener...."); //From https://github.com/tetra-fox/QMFreeze/blob/master/QMFreeze/Mod.cs
+            Logger.Msg("Adding QM listener...."); //From https://github.com/tetra-fox/QMFreeze/blob/master/QMFreeze/Mod.cs
             //string micPath = "/UserInterface/Canvas_QuickMenu(Clone)/Container/Window/MicButton";
             //while (GameObject.Find(micPath) == null)
             while (GameObject.Find("/UserInterface")?.transform.Find("Canvas_QuickMenu(Clone)/Container/Window/MicButton") == null)
@@ -59,11 +104,24 @@ namespace NearClipPlaneAdj
             listener.OnEnabled += delegate { QMopen(); };
             listener.OnDisabled += delegate { QMclosed(); };
 
-            MelonLogger.Msg("Initialized QM listener!");
+            string vrCamPath = "_Application/TrackingVolume/TrackingSteam(Clone)/SteamCamera/[CameraRig]/Neck/Camera (eye)/StackedCamera : Cam_InternalUI";
+            string desktopCamPath = "_Application/TrackingVolume/TrackingSteam(Clone)/SteamCamera/[CameraRig]/Neck/Camera (head)/Camera (eye)/StackedCamera : Cam_InternalUI";
+            
+            try
+            {
+            if (GameObject.Find(vrCamPath) != null)
+                UIcamera = GameObject.Find(vrCamPath).GetComponent<Camera>();
+            else
+                UIcamera = GameObject.Find(desktopCamPath).GetComponent<Camera>();
+            }
+            catch (System.Exception ex) { Logger.Error($"Error finding UI Camera\n" + ex.ToString()); }
+
+            Logger.Msg("Initialized QM listener + UICam Found!");
         }
 
         public void QMopen()
-        { if (!raiseNearClip) return;
+        {
+            if (!raiseNearClip.Value) return;
             var screenCamera = GetScreenCam();
             if (screenCamera is null) return;
             float clipValue = screenCamera.nearClipPlane;
@@ -73,7 +131,8 @@ namespace NearClipPlaneAdj
         }
 
         public void QMclosed()
-        { if (!raiseNearClip) return;
+        {
+            if (!raiseNearClip.Value) return;
             var screenCamera = GetScreenCam();
             if (screenCamera is null) return;
             float clipValue = screenCamera.nearClipPlane;
@@ -81,7 +140,7 @@ namespace NearClipPlaneAdj
                 ChangeNearClipPlane(oldNearClip, false);
         }
 
-        private Camera GetScreenCam() 
+        private Camera GetScreenCam()
         {
             VRCVrCamera vrCamera = VRCVrCamera.field_Private_Static_VRCVrCamera_0;
             if (!vrCamera) return null;
@@ -90,14 +149,22 @@ namespace NearClipPlaneAdj
             return screenCamera;
         }
 
-        private void ChangeNearClipPlane(float value, bool printMsg)
+        public void ChangeNearClipPlane(float value, bool printMsg)
         {
             var screenCamera = GetScreenCam();
             if (screenCamera is null) return;
             float oldvalue = screenCamera.nearClipPlane;
             screenCamera.nearClipPlane = value;
-            if(printMsg) MelonLogger.Msg($"Near plane previous: {oldvalue}, Now: {value} {(keybindsEnabled ? "- Keyboard Hotkeys: '[' - 0.0001, ']' - 0.05" : "" )}");
+            if (printMsg) Logger.Msg($"Near plane previous: {oldvalue}, Now: {value} {(keybindsEnabled.Value ? "- Keyboard Hotkeys: '[' - 0.0001, ']' - 0.05" : "")}");
             ChangePhotoCameraNearField(value);
+
+            try
+            {
+                if (changeUIcam.Value)
+                        UIcamera.nearClipPlane = value;
+            }
+            catch (System.Exception ex) { Logger.Error($"Error changing UI Camera nearclip\n" + ex.ToString()); }
+
         }
 
         private void ChangePhotoCameraNearField(float value)
@@ -134,29 +201,37 @@ namespace NearClipPlaneAdj
         System.Collections.IEnumerator SetNearClipPlane(float znear)
         {
             yield return new WaitForSecondsRealtime(15); //Wait 15 seconds after world load before setting the clipping value. Waiting for the next/first frame does not work
-            if (smallerDefault) znear = 0.001f;
+            if (smallerDefault.Value) znear = 0.001f;
             ChangeNearClipPlane(znear, true);
             oldNearClip = znear;
-            MelonLogger.Msg("Near plane adjusted after world load");
+            Logger.Msg("Near plane adjusted after world load");
         }
 
 
         public override void OnUpdate()
         {
-            if (!keybindsEnabled) return;
+            if (!keybindsEnabled.Value) return;
 
             if (Input.GetKeyDown(KeyCode.LeftBracket))
             {
-                MelonLogger.Msg(ConsoleColor.DarkCyan, "NearClip set to smallest value on keypress: 0.0001");
+                Logger.Msg(ConsoleColor.DarkCyan, "NearClip set to smallest value on keypress: 0.0001");
                 ChangeNearClipPlane(.0001f, true);
             }
             if (Input.GetKeyDown(KeyCode.RightBracket))
             {
-                MelonLogger.Msg(ConsoleColor.DarkCyan, "NearClip set to largest value on keypress: 0.05");
+                Logger.Msg(ConsoleColor.DarkCyan, "NearClip set to largest value on keypress: 0.05");
                 ChangeNearClipPlane(.05f, true);
             }
         }
 
 
+    }
+}
+
+namespace UIExpansionKit.API
+{
+    public struct LayoutDescriptionCustom
+    {
+        public static LayoutDescription QuickMenu1Column6Row = new LayoutDescription { NumColumns = 1, RowHeight = 380 / 6, NumRows = 6 };
     }
 }
