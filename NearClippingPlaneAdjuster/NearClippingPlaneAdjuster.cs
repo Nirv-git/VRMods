566caf3c68ee320bd5d41a05374f2a1d9dbd701b
Nirv-git <81605232+Nirv-git@users.noreply.github.com>
2022-01-29 01:11:33 -0600

    Update NearClippingPlaneAdjuster.cs
    Added a world blacklist option, the mod will pull a list of worlds at game load and not auto adjust the NearClip plane in those worlds.
    Added a toggle for auto adjusting the Near Clip plane. Fixes a bug where setting the NearClip from the QuickMenu when it was 0.001 and RaiseOnQuickMenu was enabled, it would get reset when closing the QM.

--- NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
+++ NearClippingPlaneAdjuster/NearClippingPlaneAdjuster.cs
@@ -7,18 +7,20 @@ using UnhollowerRuntimeLib;
 using NearClipPlaneAdj.Components;
 using System.Collections;
 using System.Linq;
+using System.Net;
+using System;
 
-[assembly: MelonInfo(typeof(NearClipPlaneAdj.NearClipPlaneAdjMod), "NearClipPlaneAdj", "1.61", "Nirvash")]
+[assembly: MelonInfo(typeof(NearClipPlaneAdj.NearClipPlaneAdjMod), "NearClipPlaneAdj", "1.63", "Nirvash")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonOptionalDependencies("ActionMenuApi")]
 
 namespace NearClipPlaneAdj
 {
-
     public class NearClipPlaneAdjMod : MelonMod
     {
         public static MelonLogger.Instance Logger;
 
+        public static MelonPreferences_Entry<bool> changeClipOnLoad;
         public static MelonPreferences_Entry<bool> keybindsEnabled;
         public static MelonPreferences_Entry<bool> smallerDefault;
         public static MelonPreferences_Entry<bool> raiseNearClip;
@@ -26,26 +28,31 @@ namespace NearClipPlaneAdj
         public static MelonPreferences_Entry<bool> AMAPI_en;
         public static MelonPreferences_Entry<bool> amapi_ModsFolder;
         public static MelonPreferences_Entry<bool> UIX_butts_QM;
+        public static MelonPreferences_Entry<bool> defaultChangeBlackList;
 
+        public static string blackList;
         public static float oldNearClip;
         public static float lastSetNearClip;
         public static Camera UIcamera;
 
         private static GameObject n05, n01, n001, n0001, QMbutt;
 
-
         public override void OnApplicationStart()
         {
             Logger = new MelonLogger.Instance("NearClipPlaneAdj", ConsoleColor.DarkYellow);
 
             MelonPreferences.CreateCategory("NearClipAdj", "NearClipPlane Adjuster");
+            changeClipOnLoad = MelonPreferences.CreateEntry<bool>("NearClipAdj", "changeClipOnLoad", true, "Change NearClip on world load");
+            UIX_butts_QM = MelonPreferences.CreateEntry("NearClipAdj", "UIX_butts_QM", true, "Place buttons in Settings QM instead of Settings Big Menu");
             keybindsEnabled = MelonPreferences.CreateEntry<bool>("NearClipAdj", "Keyboard", true, "Keyboard Shortcuts: '[' - 0.0001, ']' - 0.05");
+
+            changeUIcam = MelonPreferences.CreateEntry<bool>("NearClipAdj", "changeUIcam", true, "Change UI Camera Nearclip");
             smallerDefault = MelonPreferences.CreateEntry<bool>("NearClipAdj", "SmallerDefault", false, "Smaller Default Nearclip on World Change - 0.001 vs 0.01");
             raiseNearClip = MelonPreferences.CreateEntry<bool>("NearClipAdj", "RaiseOnQuickMenu", false, "If using smaller Default Nearclip (0.001) raise to 0.01 when Quick Menu opens.");
-            changeUIcam = MelonPreferences.CreateEntry<bool>("NearClipAdj", "changeUIcam", true, "Change UI Camera Nearclip");
+            
             AMAPI_en = MelonPreferences.CreateEntry<bool>("NearClipAdj", "AMAPI_en", true, "Action Menu API Support (Requires Restart)");
             amapi_ModsFolder = MelonPreferences.CreateEntry("NearClipAdj", "amapi_ModsFolder", false, "Place Action Menu in 'Mods' Sub Menu instead of 'Config' menu (Restert Required)");
-            UIX_butts_QM = MelonPreferences.CreateEntry("NearClipAdj", "UIX_butts_QM", true, "Place buttons in Settings QM instead of Settings Big Menu");
+            defaultChangeBlackList = MelonPreferences.CreateEntry("NearClipAdj", "defaultChangeBlackList", true, "Check a blacklist for worlds to not /auto/ change the NearClip on");
 
             ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.05", (() => ChangeNearClipPlane(.05f, true)), (butt) => { n05 = butt; butt.SetActive(!UIX_butts_QM.Value); });
             ExpansionKitApi.GetExpandedMenu(ExpandedMenu.SettingsMenu).AddSimpleButton("Nearplane-0.01", (() => ChangeNearClipPlane(.01f, true)), (butt) => { n01 = butt; butt.SetActive(!UIX_butts_QM.Value); });
@@ -67,9 +74,6 @@ namespace NearClipPlaneAdj
             }
             Menu.AddSimpleButton("Close", () => Menu.Hide());
 
-
-            Logger.Msg("Near Plane Adjust Init");
-
             Logger.Msg("Registering components...");
             ClassInjector.RegisterTypeInIl2Cpp<EnableDisableListener>();
             MelonCoroutines.Start(OnLoad());
@@ -79,6 +83,18 @@ namespace NearClipPlaneAdj
                 CustomActionMenu.InitUi();
             }
             else Logger.Msg("ActionMenuApi is missing, or setting is toggled off in Mod Settings - Not adding controls to ActionMenu");
+
+            if(defaultChangeBlackList.Value) GetBlackList();
+        }
+        private void GetBlackList()
+        {
+            try
+            {
+                string url = "https://raw.githubusercontent.com/Nirv-git/VRMods/main/NearClippingPlaneAdjuster/blacklist";
+                WebClient client = new WebClient();
+                blackList = client.DownloadString(url);
+            }
+            catch (Exception ex) { Logger.Error($"GetBlackList error\n" + ex.ToString()); }
         }
 
         public override void OnPreferencesSaved()
@@ -93,12 +109,8 @@ namespace NearClipPlaneAdj
         public IEnumerator OnLoad()
         {
             Logger.Msg("Adding QM listener...."); //From https://github.com/tetra-fox/QMFreeze/blob/master/QMFreeze/Mod.cs
-            //string micPath = "/UserInterface/Canvas_QuickMenu(Clone)/Container/Window/MicButton";
-            //while (GameObject.Find(micPath) == null)
             while (GameObject.Find("/UserInterface")?.transform.Find("Canvas_QuickMenu(Clone)/Container/Window/MicButton") == null)
                 yield return new WaitForSeconds(1f);
-            // MicControls is enabled no matter the QM page that's open, so let's use that to determine whether or not the QM is open
-            // Unless you have some other mod that removes this button then idk lol
             EnableDisableListener listener = GameObject.Find("/UserInterface")?.transform.Find("Canvas_QuickMenu(Clone)/Container/Window/MicButton").gameObject
                 .AddComponent<EnableDisableListener>();
             listener.OnEnabled += delegate { QMopen(); };
@@ -157,6 +169,7 @@ namespace NearClipPlaneAdj
             screenCamera.nearClipPlane = value;
             if (printMsg) Logger.Msg($"Near plane previous: {oldvalue}, Now: {value} {(keybindsEnabled.Value ? "- Keyboard Hotkeys: '[' - 0.0001, ']' - 0.05" : "")}");
             ChangePhotoCameraNearField(value);
+            oldNearClip = screenCamera.nearClipPlane;
 
             try
             {
@@ -164,7 +177,6 @@ namespace NearClipPlaneAdj
                         UIcamera.nearClipPlane = value;
             }
             catch (System.Exception ex) { Logger.Error($"Error changing UI Camera nearclip\n" + ex.ToString()); }
-
         }
 
         private void ChangePhotoCameraNearField(float value)
@@ -192,7 +204,7 @@ namespace NearClipPlaneAdj
                 case 2: //empty
                     break;
                 default:
-                    MelonCoroutines.Start(SetNearClipPlane(0.01f));
+                    if (changeClipOnLoad.Value) MelonCoroutines.Start(SetNearClipPlane(0.01f));
                     break;
             }
 
@@ -201,6 +213,15 @@ namespace NearClipPlaneAdj
         System.Collections.IEnumerator SetNearClipPlane(float znear)
         {
             yield return new WaitForSecondsRealtime(15); //Wait 15 seconds after world load before setting the clipping value. Waiting for the next/first frame does not work
+            if (defaultChangeBlackList.Value && RoomManager.field_Internal_Static_ApiWorld_0 != null && !blackList.Equals(null))
+            { //Check if world is blacklisted from auto change
+                if (blackList.Contains(RoomManager.field_Internal_Static_ApiWorld_0.id)) //Lazy way to check, but shouldn't be that bad?
+                {
+                    Logger.Msg(ConsoleColor.Yellow,"Not auto adjusting NearClipping plane, world blacklisted");
+                    yield break;
+                }
+            }
+            
             if (smallerDefault.Value) znear = 0.001f;
             ChangeNearClipPlane(znear, true);
             oldNearClip = znear;
@@ -223,8 +244,6 @@ namespace NearClipPlaneAdj
                 ChangeNearClipPlane(.05f, true);
             }
         }
-
-
     }
 }
 
